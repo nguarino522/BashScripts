597dbc554c881204c06633417feb5615f43f1627
Nicholas Guarino <nguarino@datto.lan>
2018-06-28 09:41:30 -0400

    asdfasd

--- find_spaces.sh
+++ find_spaces.sh
@@ -1,5 +1,14 @@
 #/bin/sh
 
-echo "Enter path to search:$PATH"
-read $PATH 
+echo "Enter path to search:"
+read PATH 
+echo $PATH
 
+for f in $PATH
+    do
+        if [ f == "* "]
+                then
+                        echo $f >> output.txt
+        fi
+    done
+done
